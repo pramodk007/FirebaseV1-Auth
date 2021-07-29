5cbc46c8e007b5b71561efb28d69bf306f7035e6
pramod kumar <pramodk6001@gmail.com>
2021-07-29 12:05:46 +0530

    added logout button to home screen.

--- app/src/main/java/com/androiddev/firebasev1_auth/HomeActivity.java
+++ app/src/main/java/com/androiddev/firebasev1_auth/HomeActivity.java
@@ -2,13 +2,32 @@ package com.androiddev.firebasev1_auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button button_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        button_logout = findViewById(R.id.button_logout);
+
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeActivity.this,StartActivity.class));
+                finish();
+                Toast.makeText(HomeActivity.this, "you have logged out", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
